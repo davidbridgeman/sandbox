0eb3a359997c20d4ff167ea01d99553fe95747c9
compsci <compsci@compsci>
2017-10-18 10:26:52 -0600

    Code for Y/N

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,10 @@
+import  java.util.Scanner;
 
 public class Runner {
 	
 	public static void main(String[] args) {
+		
+		/*
 		
 		int numberBits = 10;
 		int bite = 8;
@@ -12,6 +15,25 @@ public class Runner {
 		byteRemainder = numberBits % 8;
 	
 		System.out.println("Bits = " + numberBits + " bytes = " + byteAnswer + " remainder = " + byteRemainder);
+		
+		*/
+		
+		Scanner kbd = new Scanner(System.in);
+		System.out.println("Hello, please enter your answer (Y/N).");
+		String answer = kbd.nextLine();
+		System.out.println("You have entered: " + answer);
+		
+		if(answer.equals("Y") || answer.equals("YES")) {
+			System.out.println("You have selected yes!");
+		}
+		
+		else if(answer.equals("N") || answer.equals("NO")) {
+			System.out.println("You have selected no!");
+		}
+		
+		else {
+			System.out.println("Try again!");
+		}
 	
 	}
 	
